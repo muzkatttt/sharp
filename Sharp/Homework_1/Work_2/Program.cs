4249f75ba2e6e88c43517b83e2f492007e9d252b
Katerina <emrodionova@bk.ru>
2023-01-16 01:16:55 +0300

    +homework1_2

---
+++ Sharp/Homework_1/Work_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// написать программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел
+
+/* 2, 3, 7 -> 7;
+44 5 78 -> 78;
+22 3 9 -> 22; */
+
+
+Console.Write($"Введите число 1:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите число 2:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите число 3:  ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{ 
+    if (a > c)
+   
+    {
+        Console.WriteLine($"Наибольшее число равно {a}");
+    }
+}
+    else if (a > c)
+        {
+            Console.WriteLine($"Наибольшее число равно {a}");
+        }
+     else if (b > c)
+        {
+            Console.WriteLine($"Наибольшее число равно {b}");
+        }
+else
+        {
+            Console.WriteLine($"Наибольшее число равно {c}");
+        } 
