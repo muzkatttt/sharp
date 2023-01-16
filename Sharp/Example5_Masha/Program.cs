594f0ac7ed3b7e02e42ab5e1c691df7a29d2a0b6
Katerina <emrodionova@bk.ru>
2023-01-16 17:03:05 +0300

    save after changes

--- Sharp/Example5_Masha/Program.cs
+++ Sharp/Example5_Masha/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
+string username = Console.ReadLine()!;
 
 if(username.ToLower() == "катя")
 {
@@ -10,3 +10,4 @@ else
     Console.Write("Привет, ");
     Console.WriteLine(username);
 }
+;
