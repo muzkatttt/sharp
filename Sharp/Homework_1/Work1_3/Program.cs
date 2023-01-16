f8877c3859597b37fd224121cf95776db22a91c0
Katerina <emrodionova@bk.ru>
2023-01-16 11:03:32 +0300

    added homework1_3

--- Sharp/Homework_1/Work1_3/Program.cs
+++ Sharp/Homework_1/Work1_3/Program.cs
@@ -4,4 +4,14 @@
 -3 -> нет
 7 -> нет */
 
-Console.WriteLine("Hello, World!");
+Console.Write($"Введите число:  ");
+double a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 ==0)
+{
+    Console.WriteLine($"{a} является четным числом");
+}
+else
+{
+    Console.WriteLine($"{a} является нечетным числом");
+}
