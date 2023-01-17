724232c6afca5fe4b1ee634317f9f54c04d32122
Katerina <emrodionova@bk.ru>
2023-01-17 20:21:59 +0300

    added задачи про максимум из 9 чисел из лекции

---
+++ Sharp/Example8_IntroMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿// найти наибольшее из 9 чисел
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
+
