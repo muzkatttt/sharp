f740ac7ba5b0a3dedcfc277a17347cf7321bdb33
Katerina <emrodionova@bk.ru>
2023-01-25 09:34:23 +0300

    added Sem4_1

---
+++ Sharp/Sem4_1Razr/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 26. Напишите программу, которая на вход принимает число
+и выдает количество цифр в числе
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int Count(int arg)
+{
+    int count = 0;
+    while(arg > 0)
+    {
+        arg /= 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(Count(a));
