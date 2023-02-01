a936234497d1ec77e34052141bc1db47be3e5d47
Katerina <emrodionova@bk.ru>
2023-02-01 03:28:57 +0300

    added work 38

--- Sharp/Homework5/work38/Program.cs
+++ Sharp/Homework5/work38/Program.cs
@@ -3,12 +3,42 @@
 элементом массива
 */
 
-Random rand = new Random();
-double[] numbers = new double[10];
-for (int i = 0; i < numbers.Length; i++)
+double Difference(double[] arr)
 {
-    numbers[i] = rand.NextDouble() + new Random().Next(1, 100);
+    double min = arr[0];
+    double max = arr[0];
+    double diff = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+        else if (arr[i] > max) max = arr[i];
+    }
+    diff = max - min;
+    Console.WriteLine($"max = {max}; min = {min}");
+    Console.WriteLine($"Разница = {diff}");
+    return diff;
 }
-Console.Clear();
-Console.WriteLine(String.Join(" ", numbers));
+
+double[] RandomArray(double minValue, double maxValue)
+{
+    Random nums = new Random();
+    double[] numbers = new double[3];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = nums.NextDouble() + new Random().Next(1, 10);
+    }
+    return numbers;
+}
+
+double[] myArray = RandomArray(0, 10);
+Console.WriteLine(String.Join(" ", myArray));
+Difference(myArray);
+
+
+
+
+
+
+
+
 
---
+++ Sharp/Sem4_4/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Напишите программу, которая выводит массив из 8 элементов,
+заполненный нуляи и единицами в случайном порядке
+[1, 0, 1, 1, 0, 1, 0, 0]
+*/
+
+void PrintArray(int[] array)
+{
+    string ourArray = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        ourArray += array[i];
+        if (i != array.Length - 1) ourArray += ", ";
+    }
+    Console.WriteLine(ourArray);
+}
+
+// можно так записать код
+// void main()
+// {
+//     int[] intArray = new int[8];
+//     for (int i = 0; i < intArray.Length; i++)
+//     {
+//         int randInt = new Random().Next(0, 2); // ставим 2 - т.к. значение не входит в интервал!
+//         intArray[i] = randInt;
+//     }
+//     PrintArray(intArray);
+// }
+
+// main();
+
+int[] getArray()
+{
+    int[] intArray = new int[8];
+
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        int randInd = new Random().Next(0, 2);
+        intArray[i] = randInd;
+    }
+    return intArray;
+}
+
+int[] array = getArray();
+Console.WriteLine(String.Join(", ", array));
