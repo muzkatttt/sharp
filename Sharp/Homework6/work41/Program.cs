1c38587ff2782f0328dcd4b5cc01c913264ac132
Katerina <emrodionova@bk.ru>
2023-02-01 22:06:31 +0300

    decided tasks

--- Sharp/Homework6/work41/Program.cs
+++ Sharp/Homework6/work41/Program.cs
@@ -4,3 +4,40 @@
 -1, -7, 567, 89, 223-> 3
 */
 
+int[] ReadArray(int n)
+{
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        result[i] = int.Parse(Console.ReadLine()!);
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Пользователем введены числа {count} раз");
+}
+
+int[] array = ReadArray(20);
+PrintArray(array); 
+
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+ 
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
+ 
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
--- Sharp/Homework6/work43/Program.cs
+++ Sharp/Homework6/work43/Program.cs
@@ -3,4 +3,52 @@
 x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+y1 = k1x + b1; y2 = k2x + b2;
+x = (b2 - b1)/(k1 - k2); y = k1x + b1;
 */
+
+void CalculationXY() //double abscissa, double ordinate
+{
+    Console.Clear();
+    Console.WriteLine("Введите значение b1: ");
+    double b1 = double.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Введите значение k1: ");
+    double k1 = double.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Введите значение b2: ");
+    double b2 = double.Parse(Console.ReadLine()!);
+
+    Console.WriteLine("Введите значение k2: ");
+    double k2 = double.Parse(Console.ReadLine()!);
+
+    double x = 0;
+    double y = 0;
+    
+    if ((k1 == k2) && (b1 != b2))
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else if ((k1 == k2) && (b1 == b2))
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = (k1 * x) + b1;
+        Console.WriteLine($"Координаты точки пересечения прямых x = {x}, y = {y}");
+    }
+}
+
+CalculationXY();
+
+// int ReadConsol(string msg) // считывание с консоли информации пользователя
+// {
+//     Console.Write(msg);
+//     return int.Parse(Console.ReadLine()!);
+// }
+
+// Console.Clear();
+// int a = ReadConsol("Введите число А: ");
+// int b = ReadConsol("Введите натуральное число B: ");
