537f20c65401b28a0e8d735fe3604deeb75b2340
Katerina <emrodionova@bk.ru>
2023-02-06 14:33:41 +0300

    доработала 50 задачу

--- Sharp/Homework7/work50/Program.cs
+++ Sharp/Homework7/work50/Program.cs
@@ -17,7 +17,7 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue+1);
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     }
     return result;
@@ -34,23 +34,42 @@ void PrintArray(int[,] inArray)
     }
 }
 
-void FindElement(int[,] myArray)
+void FindElement(int[,] myArray, int row, int col)
 {
+    int find = Find(); // вызов метода из метода
     for (int i = 0; i < myArray.GetLength(0); i++)
     {
         for (int k = 0; k < myArray.GetLength(1); k++)
         {
-            if (myArray[i,k] == find[i,k]);
-
+            if (find < myArray[i, k] || find > myArray[i, k])
+            {
+                Console.WriteLine($"Заданный элемент {find} отсутствует в массиве");
+            }
+            else if (myArray[i, k] == find)
+            {
+                Console.WriteLine($"Значение элемента массива {myArray[i, k]}, индекс строки {i}, индекс столбца {k}");
+            }
         }
     }
 }
+void Main()
+{
+    Console.Clear();
+    Console.Write("Введите кол-во строк: ");
+    int row = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите кол-во столбцов: ");
+    int col = int.Parse(Console.ReadLine()!);
+    int[,] array = GetArray(row, col, 1, 10);
+    PrintArray(array);
+    FindElement(array, row, col);
+}
 
-Console.Clear();
-Console.Write("Введите кол-во строк: ");
-int row = int.Parse(Console.ReadLine()!);
-Console.Write("Введите кол-во столбцов: ");
-int col = int.Parse(Console.ReadLine()!);
 
-int[,] array = GetArray(row, col, 4, 13);
-PrintArray(array);
+int Find()
+{
+    Console.Write("Введите значение: ");
+    int find = int.Parse(Console.ReadLine()!);
+    return find;
+}
+
+Main();
