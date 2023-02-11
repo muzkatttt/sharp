417e57c5340aafaa3c8fdc4f298afdff6d2d1073
Katerina <emrodionova@bk.ru>
2023-02-11 21:18:38 +0300

    HW8 work62

--- Sharp/Homework8/work60/Program.cs
+++ Sharp/Homework8/work60/Program.cs
@@ -7,3 +7,45 @@
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1) */
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(10, 100);
+    }
+}
+
+void OutputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void ShuffleArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        int randomItem = random.Next(i);
+        int shuffledElement = array[randomItem];
+        array[randomItem] = array[i];
+        array[i] = shuffledElement;
+    }
+}
+
+void Main(string[] args)
+{
+    int[] array = new int[8];
+    FillArray(array);
+    Console.WriteLine("Исходный массив");
+    OutputArray(array);
+    ShuffleArray(array);
+    Console.WriteLine("\n Перемешанный массив");
+    OutputArray(array);
+}
+
+Main(args);
--- Sharp/Homework8/work62/Program.cs
+++ Sharp/Homework8/work62/Program.cs
@@ -7,3 +7,110 @@
 11 16 15 06
 10 09 08 07 
 */
+
+void Main()
+{
+    int[,] array = new int[4, 4];
+    int size = 4;
+    int i = 0;
+    int j = 0;
+    int number = 0;
+
+    for (j = 0, i = 0; j < size; j++)
+
+        array[i, j] = number;
+    number++;
+
+    for (i = 0, j = size - 1; i < size; i++)
+        array[i, j] = i + 1;
+
+    for (j = size - 1, i = size - 1; j >= 0; j--)
+        array[i, j] = size + 1;
+
+    int count = 3;
+    int c = 0;
+
+    i = size - 1;
+    j = 0;
+
+    for (int x = size - 3; x > 0; x -= 2)
+    {
+        if (size % 2 == 0 & x == 1)
+            c = 1;
+        else
+            c = 0;
+
+        while (c < 2)
+        {
+            switch (count)
+            {
+                case 1: //Down
+                    for (int t = 0; t < x; t++)
+                    {
+                        i++;
+                        array[i, j] = size + 1;
+                    }
+                    count++;
+                    break;
+                case 2: //Left
+                    for (int t = 0; t < x; t++)
+                    {
+                        j--;
+                        array[i, j] = size + 1;
+                    }
+                    count++;
+                    break;
+                case 3: //Up
+                    for (int t = 0; t < x; t++)
+                    {
+                        i--;
+                        array[i, j] = 1;
+                    }
+                    count++;
+                    break;
+                case 4: //Right
+                    for (int t = 0; t < x; t++)
+                    {
+                        j++;
+                        array[i, j] = 1;
+                    }
+                    count = 1;
+                    break;
+            }
+            c++;
+        }
+    }
+
+    for (int ii = 0; ii < size; ii++)
+    {
+        for (int jj = 0; jj < size; jj++)
+            Console.Write($"{array[ii, jj]} \t");
+        Console.WriteLine();
+    }
+
+    Console.ReadLine();
+}
+
+Main();
+
+
+
+
+
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]}\t ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Console.WriteLine();
+// int[,] array = new int[4, 4];
+// PrintArray(array);
