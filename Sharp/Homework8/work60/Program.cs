697744389bcfdf9ba0013503b3009a8c6182e0de
Katerina <emrodionova@bk.ru>
2023-02-12 14:22:51 +0300

    ended work60

--- Sharp/Homework8/work60/Program.cs
+++ Sharp/Homework8/work60/Program.cs
@@ -8,51 +8,82 @@
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1) */
 
-
-int[] RandomArray(int nums, int minValue, int maxValue) // заполняю массив элементами
+void RandomArray(int[,,] array)
 {
-    int[] result = new int[nums];
-
-    for (int i = 0; i < nums; i++)
+    int[] result = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    int number;
+    for (int i = 0; i < result.GetLength(0); i++)
     {
+        int minValue = 10;
+        int maxValue = 99;
         result[i] = new Random().Next(minValue, maxValue + 1);
+        number = result[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (result[i] == result[j])
+                {
+                    result[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = result[i];
+                }
+                number = result[i];
+            }
+        }
     }
-    return result;
+    int count = 0;
+    for (int y = 0; y < array.GetLength(0); y++)
+    {
+        for (int x = 0; x < array.GetLength(1); x++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                array[x, y, z] = result[count];
+                count++;
+            }
+        }
+    }
 }
 
-void PrintArray(int[] array)
+void PrintArray(int[,,] array2)
 {
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < array2.GetLength(0); i++)
     {
-        Console.Write($"{array[i]}\t ");
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            for (int q = 0; q < array2.GetLength(2); q++)
+            {
+                Console.Write($"Значение ячейки {array2[i, j, q]} индекс [{i}, {j}, {q}]\t ");
+            }
+            Console.WriteLine();
+        }
+        // Console.WriteLine();
     }
-    Console.WriteLine();
 }
 
-
-int[] Shuffle(int[] array)
+int SizeInput(string input)
 {
-    int random = new Random().Next();
-    for (int i = array.Length; i > 1; i--)
-    {
-        int j = new Random().Next(i);
-        int temp = array[j];
-        array[j] = array[i - 1];
-        array[i - 1] = temp;
-    }
-    return array;
+    Console.Write(input);
+    int size = int.Parse(Console.ReadLine()!);
+    return size;
 }
 
 void Main()
 {
     Console.Clear();
-    int[] array = RandomArray(nums: 8, minValue: 10, maxValue: 100);
-
-    PrintArray(array);
-    Console.WriteLine();
-    Shuffle(array);
-    PrintArray(array);
-
+    Console.WriteLine($"Введите размер массива X x Y x Z: ");
+    int x = SizeInput("Введите X: ");
+    int y = SizeInput("Введите Y: ");
+    int z = SizeInput("Введите Z: ");
+    Console.WriteLine($" ");
+    int[,,] arrayX = new int[2, 2, 2];
+    RandomArray(arrayX);
+    PrintArray(arrayX);
 }
 Main();
+
+
+
+
+
