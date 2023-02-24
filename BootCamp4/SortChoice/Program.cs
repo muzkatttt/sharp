56b56aa6d43dd1d4d4d621d7c2fda578173f96ba
Katerina <emrodionova@bk.ru>
2023-02-24 20:24:02 +0300

    added BootCamp4 & JavaScript

---
+++ BootCamp4/SortChoice/Program.cs
@@ -0,0 +1,25 @@
+﻿public static class Sorting
+{
+
+    /// <summary>
+    /// Сортировка методом выбора
+    /// </summary>
+    /// <param name="collection">Исходный массив</param>
+    /// <returns>Отсортированный массив массив</returns>
+    public static int[] SortSelection(this int[] collection)
+    {
+        int size = collection.Length;
+        for (int i = 0; i < size - 1; i++)
+        {
+            int pos = i;
+            for (int j = i + 1; j < size; j++)
+            {
+                if (collection[j] < collection[pos]) pos = j;
+            }
+            int temp = collection[i];
+            collection[i] = collection[pos];
+            collection[pos] = temp;
+        }
+        return collection;
+    }
+}
