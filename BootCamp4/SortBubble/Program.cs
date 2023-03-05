df03a87f475e3491594b138b405aa2241ef033fc
Katerina <emrodionova@bk.ru>
2023-03-05 18:17:48 +0300

    added task ParallSupplyMatrix

---
+++ BootCamp4/SortBubble/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Cортировка пузырьком
+Начальный массив: [3, 1, 5, 0, 7, 9, 8]
+*/
+Console.WriteLine("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите значения массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", array) + "]");
+}
